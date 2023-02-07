6dc730e5da9ae13dc1cc3918bf50e3ced32fb387
Hüseyin Alav <alavhasan72892@gmail.com>
2023-02-07 22:59:47 +0300

    inMemory cache introduction

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        #region Set and Get
+
+        //[HttpGet("/get")]
+        //public string GetName()
+        //{
+        //    if (_memoryCache.TryGetValue<string>("name", out string name))
+        //    {
+        //        return _memoryCache.Get<string>("name");
+        //    }
+
+        //    return null;
+        //}
+
+        //[HttpGet("/set/{name}")]
+        //public void SetName(string name)
+        //{
+        //    _memoryCache.Set("name", name);
+        //}
+
+        #endregion
+
+        [HttpGet("/set")] 
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("get")]
+        public DateTime GetDate()
+        {
+            return _memoryCache.Get<DateTime>("date");
+        }
+
+    }
+}
--- InMemory.Caching/Program.cs
+++ InMemory.Caching/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMemoryCache(); // Memory Cache çalýþmak istendiði zaman eklenmesi gerekiyor.
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
